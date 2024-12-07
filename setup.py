f900bcba80588af04022eb21d0eca8d1383205e1
Marcus <marcusongkiansiong@gmail.com>
2024-12-07 15:32:09 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lala",  # Package name
+    version="0.1.0",  # Initial version number
+    packages=find_packages(),  # Automatically find all packages in the directory
+)
+
